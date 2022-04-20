425132b95b9e8c233ecf17dcd6e2ba7a2cf60430
bonnie <arlolocos@gmail.com>
2022-04-19 21:35:00 -0400

    update account is fully functional

--- components/Settings/Filter.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from "react";
-
-const Filter = (props) => {
-  return <span></span>;
-};
-
-export default Filter;
--- components/Settings/FilterList.jsx
+++ components/Settings/FilterList.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-// import Filter from "./Filter.jsx";
 
 const FilterList = (props) => {
 
